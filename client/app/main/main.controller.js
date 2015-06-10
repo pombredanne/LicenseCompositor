e90dfa038ce475c08a618c921887ef68585838b3
Ivan Ermilov <earthquakesan@gmail.com>
2015-06-11 00:06:54 +0200

    first working version -- mockup

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,15 +3,63 @@
 angular.module('licenseCompositorApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
-    $scope.selectedLicense1 = null;
-    $scope.selectedLicense2 = null;
-    $scope.selectedLicense3 = null;
+    $scope.licenses = [];
+    $scope.licenses[0] = {
+      'selectedLicense': 'CC-BY-4.0'
+    };
+    $scope.licenses[1] = {
+      'selectedLicense': 'CC-BY-NC-SA-4.0'
+    };
+    $scope.licenses[2] = {
+      'selectedLicense': 'CC-BY-SA-4.0'
+    };
+    $scope.store = null;
+    $scope.license = null;
 
-    $scope.$watch("selectedLicense1", function(newValue, oldValue) {
-      console.log(oldValue);
-      console.log(newValue);
-      //Fetch the license from the data folder and render it!
-    });
+    $scope.getLicenses = function() {
+      $scope.licenses.forEach(function(license) {
+        if(license['selectedLicense']) {
+          var licenseBaseUrl = '/assets/licenses/';
+          var licenseUrl = licenseBaseUrl + license['selectedLicense'] + '.ttl';
+          $http.get(licenseUrl).success(function(licenseTurtleString) {
+            new rdfstore.Store(function(err, store) {
+              store.load('text/turtle', licenseTurtleString, function(err, results) {
+                if(err) {
+                  console.log('Error during parsing! ' + err);
+                }
+                //Get requirements
+                store.execute('SELECT ?requirement {?s <http://creativecommons.org/ns#requires> ?requirement}', function(err, results) {
+                  if(err) {
+                    console.log('Error during query execution! ' + err);
+                  }
+                  license.requirements = results;
+                });
+                //Get permissions
+                store.execute('SELECT ?permission {?s <http://creativecommons.org/ns#permits> ?permission}', function(err, results) {
+                  if(err) {
+                    console.log('Error during query execution! ' + err);
+                  }
+                  license.permissions = results;
+                });
+                //Get prohibitions
+                store.execute('SELECT ?prohibition {?s <http://creativecommons.org/ns#prohibits> ?prohibition}', function(err, results) {
+                  if(err) {
+                    console.log('Error during query execution! ' + err);
+                  }
+                  license.prohibitions = results;
+                });
+              });
+            });
+          });
+        }
+      });
+    };
+
+    $scope.getLicenses();
+
+    $scope.$watch('licenses', function(licenses, oldLicenses) {
+      $scope.getLicenses();
+    }, true);
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
