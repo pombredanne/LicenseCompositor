7fb06ad88bcd544177d8f06109442c93acdaa11c
Ivan Ermilov <earthquakesan@gmail.com>
2015-06-10 16:44:34 +0200

    simple 3 licenses compositor -- prototype

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,15 @@
 angular.module('licenseCompositorApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
+    $scope.selectedLicense1 = null;
+    $scope.selectedLicense2 = null;
+    $scope.selectedLicense3 = null;
+
+    $scope.$watch("selectedLicense1", function(newValue, oldValue) {
+      console.log(oldValue);
+      console.log(newValue);
+      //Fetch the license from the data folder and render it!
+    });
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
